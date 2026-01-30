73ae21cb10f942fdab813a73601ea54241d52bb8
Muhammad Taha <muhammadtaha1780@gmail.com>
2026-01-30 15:52:02 +0500

    decrease per page comment limit

--- backend/src/comments/dto/list-comments-query-payload.dto.ts
+++ backend/src/comments/dto/list-comments-query-payload.dto.ts
@@ -1,11 +1,13 @@
 import { IsOptional, IsInt, Min } from 'class-validator';
 import { Type } from 'class-transformer';
 import { VALIDATION_MESSAGES } from '../../lib/constants';
+import { PaginationQueryDto } from '../../posts/dto/pagination-query-input.dto';
 
-export class ListCommentsQueryDto {
+export class ListCommentsQueryDto extends PaginationQueryDto {
   @IsOptional()
   @Type(() => Number)
   @IsInt()
   @Min(1, { message: VALIDATION_MESSAGES.POST_ID_INVALID })
   postId?: number;
 }
+
